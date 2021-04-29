810780ccce19c1b243457f68159413d569e4bc96
colacilloc <ccolacil@gmail.com>
2021-04-29 22:06:17 +0200

    first commit

---
+++ CodeCalisthenics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeCalisthenics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RoadRepair.MainEntrance(args);
+
+            /*int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            FizzBuzz.Execute(n);*/
+        }
+    }
+}
