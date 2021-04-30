30f44295a01b217ffbca40056d0bafd82644178f
colacilloc <ccolacil@gmail.com>
2021-04-30 20:41:22 +0200

    .

--- CodeCalisthenics/RoadRepair/Result.cs
+++ CodeCalisthenics/RoadRepair/Result.cs
@@ -5,6 +5,15 @@ namespace CodeCalisthenics.RoadRepair
 {
     public class Result
     {
+        /**
+         * A Number of points along the highway are in need of repair. An equal number of crews are available, stationed at various points along the highway.
+         * They must move along the highway to reach an assigned point. Given that one crew must be assigned to each job,
+         * what is the minimum total amount of distance traveled by all crews before they can begin to work ?
+         *
+         * For example, given crews at points {1,3,5}, one possible minimum assignment would be {1->3, 3->5, 5->7} for a total of 6 units traveled.
+         * Function Description:
+         * Complete the function GetMinCost in the editor below. The function should return the minimum total distance traveled as an integer. 
+         */
         public static long GetMinCost(List<int> crew_id, List<int> job_id)
         {
             var crewMembers = new List<int>(crew_id.OrderBy(x => x));
