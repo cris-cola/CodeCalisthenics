66ad178d61f17dc73ddc1df047f5dfdcdb1ab39e
colacilloc <ccolacil@gmail.com>
2021-04-30 17:56:40 +0200

    .

--- CodeCalisthenics/RoadRepair/RoadRepair.cs
+++ CodeCalisthenics/RoadRepair/RoadRepair.cs
@@ -8,17 +8,15 @@ namespace CodeCalisthenics.RoadRepair
     {
         public static void MainEntrance(string[] args)
         {
-            int crewIdCount = Convert.ToInt32(5);//Convert.ToInt32(Console.ReadLine().Trim());
+            int crewIdCount = Convert.ToInt32(5);
 
-            List<int> crew_id = new List<int> { 5, 3, 1, 6, 9 };
-                                            // 1, 3, 5, 6, 9 
+            List<int> crewId = new List<int> { 5, 3, 1, 6, 9 };
             
             int jobIdCount = Convert.ToInt32(5);
 
             List<int> jobId = new List<int> { 9, 8, 3, 15, 1 };
-                                             //1, 3, 8, 9, 15 
             
-            var result = Result.GetMinCost(crew_id, jobId);
+            var result = Result.GetMinCost(crewId, jobId);
 
             Console.Write(result);
         }
